fb5d3f001eebbe5d6f0967d1bb99afa5f6e375b9
Dani <dustyglass@gmail.com>
2024-08-20 17:56:53 -0400

    working with multiple repeated functions

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-var likeCount = 0;
-var likeCountText = document.getElementById("likeCount");
-var likeBtn = document.getElementById("likeBtn");
-likeBtn.addEventListener("click", clickLike);
+var likeCount1 = 0;
+var likeCountText1 = document.getElementById("likeCount1");
+var likeBtn1 = document.getElementById("likeBtn1");
+likeBtn1.addEventListener("click", clickLike1);
 
-function clickLike() {
-    likeCount += 1
-    likeCountText.innerText = `${likeCount} like(s)`
+function clickLike1() {
+    likeCount1 += 1
+    likeCountText1.innerText = `${likeCount1} like(s)`
+}
+
+var likeCount2 = 0;
+var likeCountText2 = document.getElementById("likeCount2");
+var likeBtn2 = document.getElementById("likeBtn2");
+likeBtn2.addEventListener("click", clickLike2);
+
+function clickLike2() {
+    likeCount2 += 1
+    likeCountText2.innerText = `${likeCount2} like(s)`
+}
+
+var likeCount3 = 0;
+var likeCountText3 = document.getElementById("likeCount3");
+var likeBtn3 = document.getElementById("likeBtn3");
+likeBtn3.addEventListener("click", clickLike3);
+
+function clickLike3() {
+    likeCount3 += 1
+    likeCountText3.innerText = `${likeCount3} like(s)`
 }
