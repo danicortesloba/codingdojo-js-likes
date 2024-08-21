35576ddd405a2649e07a9613a16d1c29ef7cbedd
Dani <dustyglass@gmail.com>
2024-08-20 21:42:16 -0400

    Refactor functions

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
-/*
-var likeCount1 = 0;
-var likeCountText1 = document.getElementById("likeCount1");
-var likeBtn1 = document.getElementById("likeBtn1");
-likeBtn1.addEventListener("click", clickLike1);
+let countOne = 0
+let countTwo = 0
+let countThree = 0
 
-function clickLike1() {
-    likeCount1 += 1
-    likeCountText1.innerText = `${likeCount1} like(s)`
+function clickLike (element) {
+    if(element === document.getElementById("likeBtn1")){
+        let text = document.getElementById("1")
+        countOne ++
+        text.innerText = `${countOne} likes(s)`
+    } else if (element === document.getElementById("likeBtn2")){
+        let text = document.getElementById("2")
+        countTwo ++
+        text.innerText = `${countTwo} likes(s)`
+    }
+    else if (element === document.getElementById("likeBtn3")){
+        let text = document.getElementById("3")
+        countThree ++
+        text.innerText = `${countThree} likes(s)`
+    }
 }
 
-var likeCount2 = 0;
-var likeCountText2 = document.getElementById("likeCount2");
-var likeBtn2 = document.getElementById("likeBtn2");
-likeBtn2.addEventListener("click", clickLike2);
-
-function clickLike2() {
-    likeCount2 += 1
-    likeCountText2.innerText = `${likeCount2} like(s)`
-}
-
-var likeCount3 = 0;
-var likeCountText3 = document.getElementById("likeCount3");
-var likeBtn3 = document.getElementById("likeBtn3");
-likeBtn3.addEventListener("click", clickLike3);
-
-function clickLike3() {
-    likeCount3 += 1
-    likeCountText3.innerText = `${likeCount3} like(s)`
-}
-*/
-var likeCount = 0;
-var buttons = document.getElementsByClassName("likeBtn");
-for(const button of buttons){
-    button.addEventListener("click", function() {
-        var buttonTexts = document.getElementsByClassName("textCount")
-        for(const text of buttonTexts){
-            if(button.id.includes(text.id)){
-                console.log("hello")
-                likeCount += 1;
-                text.innerHTML = likeCount + "likes"
-            }
-        }
-})
-}
